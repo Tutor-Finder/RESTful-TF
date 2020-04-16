53c6f1c86d3f6252d0bd3bb4eed6a7ddbd6a6f23
Matthew Garber <matgar1997@gmail.com>
2020-04-16 03:57:09 -0400

    bug fixes

--- authentication/auth.service.js
+++ authentication/auth.service.js
@@ -1,51 +1,18 @@
-const pool = require("../config/database");
+const database = require("../config/database");
 
 function getUserByRefreshToken(refreshToken, callBack) {
-
-    // connect to database
-    pool.getConnection(function(err) {
-
-        // Somthing went wrong connecting to mysql database
-        if(err) throw err;
-        
-        // run query to insert data into database
-        var sql = `select * from Users where refreshToken = ?`;
-        pool.query(sql, 
-            [refreshToken],
-            (error, results, fields) => {
-                if(error) {
-                    // Somthing went wrong running the sql command
-                    return callBack(error);
-                }
-                // Data added to table
-                return callBack(null, results);
-            });
-    });
+    sql = `select * from Users where refreshToken = ?`
+    my_values = [refreshToken]
+    database.queryDB(sql, my_values, callBack)
 }
 
 function setRefreshToken(id, refreshToken, callBack) {
-    // connect to database
-    pool.getConnection(function(err) {
-
-        // Somthing went wrong connecting to mysql database
-        if(err) throw err;
-        
-        // run query to insert data into database
-        var sql = `update Users set refreshToken=? where id = ?`;
-        pool.query(sql, 
-            [
-                refreshToken,
-                id
-            ],
-            (error, results, fields) => {
-                if(error) {
-                    // Somthing went wrong running the sql command
-                    return callBack(error);
-                }
-                // Data added to table
-                return callBack(null, results);
-            });
-    });
+    sql = `update Users set refreshToken=? where id = ?`
+    my_values = [
+                    refreshToken,
+                    id
+                ]
+    database.queryDB(sql, my_values, callBack)
 }
 
 module.exports = {
--- core_api/users/user.router.js
+++ core_api/users/user.router.js
@@ -2,16 +2,12 @@ const userController = require("./user.controller");
 const auth = require("../../authentication/auth.controller");
 const router = require("express").Router();
 
-
-// const { checkToken, checkTokenUser } = require("../../auth/token_validation")
-
 // routers
 router.post("/", userController.createUser); 
 router.get("/", auth.validate_token, userController.getUsers);   
 router.get("/:id", auth.validate_token, userController.getUserById); 
 //router.patch("/", auth.validate_token, userController.updateUsers); 
 // router.delete("/", checkTokenUser, userController.deleteUser); 
-// router.post("/login", userController.login);
 
 // make router availble to other modules
 module.exports = router;
